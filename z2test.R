1ad6a5a60bb8cf304693ed483190b85b2eeb31bc
Nicole Radziwill <NicoleRadziwill@users.noreply.github.com>
2017-07-25 18:09:06 -0400

    Update z2test.R

--- z2test.R
+++ z2test.R
@@ -20,7 +20,7 @@ z2.test <- function(x1,n1,x2,n2,conf.level=0.95,
    p.val <- pnorm(ts.z) # defaults to alternative="less"
    if(alternative=="two.sided") {
           if(p.val > 0.5) {
-             p.val <- p.val - 0.5  
+             p.val <- 1 - p.val  
           }
           p.val <- p.val * 2
       if(p.val > 1) { p.val = 1 }
